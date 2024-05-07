6ab6c23ddee56460d6516bfa75c22739dc07470d
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-07 08:46:23 -0400

    exemplo template literals

---
+++ template-literals.js
@@ -0,0 +1,6 @@
+let nome = "Marcely Arruda";
+let idade = 17;
+
+//Usando template literals para criar uma string com variáveis
+let saudacao = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.`;
+console.log(saudacao); //Saída: "Olá, meu nome é Marcely Arruda e eu tenho 17 anos."

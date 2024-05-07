22c7b43a161c5f93dc2b026e1c338bb3e21036e4
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-07 08:39:27 -0400

    exemplo concanetacao2

---
+++ concanetacao2.js
@@ -0,0 +1,4 @@
+let nome = "Marcely";
+let idade = 17;
+let saudacao = "Olá, meu nome é " + nome + "e eu tenho " + idade + " anos.";
+console.log(saudacao); //Saída: "Olá, meu nome é Marcely e eu tenho 17 anos."

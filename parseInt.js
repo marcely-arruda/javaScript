8fd7dc0bf68fe80bcbb016a871d61655a373eec9
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-07 08:45:35 -0400

    exemplo parse int

---
+++ parseInt.js
@@ -0,0 +1,8 @@
+let stringParaNumero = "100";
+console.log(stringParaNumero); //Saída: "100"
+console.log(typeof(stringParaNumero)); //Saída: string
+console.log(""); //Saída: "100"
+//Conversão da String "stringParaNumero" para number
+let numero = parseInt(stringParaNumero);
+console.log(numero); //Saída: 100
+console.log(typeof(numero)); //Saída: number 

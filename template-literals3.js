4b1307efc84866e657d33529aadced91ea6dd6c2
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-07 08:47:45 -0400

    exemplo template literals3

---
+++ template-literals3.js
@@ -0,0 +1,16 @@
+let textoMultilinha = `
+Esta é uma string
+que ocupa múltiplas linhas
+e você não precisa de preocupar 
+com quabras de linhas ou concatenação.
+`;
+console.log(textoMultilinha);
+/*
+Saída:
+"
+Esta é uma string
+que ocupa múltiplas linhas
+e você não precisa de preocupar 
+com quabras de linhas ou concatenação.
+"
+*/

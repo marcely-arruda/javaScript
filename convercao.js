11af0020a851920477be3502a2a3c2d6f4672122
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-07 08:41:10 -0400

    exemplo conversao

---
+++ convercao.js
@@ -0,0 +1,7 @@
+let concatenacao = "1.1" + "1.1";
+let converterParaNumero = +"1.1";
+console.log(concatenacao); //"1.11.1"
+console.log(typeof(concatenacao)); //string
+console.log("");
+console.log(converterParaNumero); // 1.1
+console.log(typeof(converterParaNumero)); //number

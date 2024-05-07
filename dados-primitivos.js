0949c4f8faffa701085794f13e810868f485f9b6
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-07 08:44:25 -0400

    exemplo dados primitivos

---
+++ dados-primitivos.js
@@ -0,0 +1,13 @@
+let nome = "Wanderson"; // Tipo String
+let verdadeiro = true; // Tipo Boolean
+let falso = false; //Tipo Boolean 
+let numero = 40; // Tipo Number
+let reais = 25.99; // Tipo Number
+let nulo = null; // Tipo Null
+let indefinido; // Tipo Undefined
+let NotANumber = 0 / 0; // Tipo NaN
+let objeto = {
+    name:"Wanderson",
+    age: 40,
+    Profession: "Teacher"
+}

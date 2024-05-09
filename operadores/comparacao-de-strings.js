cc9976610d163bf285792dd22772fe1e5ae21f71
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-09 09:07:57 -0400

    exemplo comparação de strings

---
+++ operadores/comparacao-de-strings.js
@@ -0,0 +1,8 @@
+let stringsIguais     = "Wanderson" == "Wanderson"; //true
+let stringsDiferentes = "wanderson" == "wanderson"; //false
+let stringMenor       = "A" < "B"; //true
+let stringMaior       = "C" > "D";
+console.log(`Wanderson == Wanderson : ${stringsIguais}`);
+console.log(`wanderson == Wanderson : ${stringsDiferentes}`);
+console.log(`A < B : ${stringMenor}`);
+console.log(`C > D : ${stringMaior}`);

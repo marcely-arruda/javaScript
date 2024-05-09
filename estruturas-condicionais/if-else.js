3537cbe4395a6b749b0016ebba65edd8c4aa0aa5
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-09 09:08:33 -0400

    exemplo de estruturas condicionais

---
+++ estruturas-condicionais/if-else.js
@@ -0,0 +1,6 @@
+let diaDaSemana = "quarta-feira";
+if (diaDaSemana === "sábado" || diaDaSemana === "Domingo") {
+    console.log("É fim de semana!");
+} else{
+    console.log("É dia de semana.");
+}
---
+++ estruturas-condicionais/if.js
@@ -0,0 +1,7 @@
+let hora = 15;
+
+if (hora < 12) {
+    console.log("Bom dia!");
+}
+// O resultante do código continua independentemente se a condição for verdadeiro ou não.
+console.log("Dinheiro não traz felicidade. Me dê o seu e seja feliz!");

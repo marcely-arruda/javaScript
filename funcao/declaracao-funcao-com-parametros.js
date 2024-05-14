d96f7832ffea04ed62651091fbc48390e445a035
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-14 11:18:30 -0400

    adicionar exemplo de declaração

---
+++ funcao/declaracao-funcao-com-parametros.js
@@ -0,0 +1,7 @@
+function nomeDaFuncao(parametro01, parametro02) {
+    //Corpo da função
+    //Pode conter uma ou mais instruções
+    return resultado; //Opcional
+}
+//Chamada de função
+nomeDaFuncao(parametro01, parametro02);

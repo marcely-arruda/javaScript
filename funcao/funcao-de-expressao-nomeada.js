9834e9652599254884030e0a9be6522ad138df0a
marcely-arruda <arrudamarcely460@gmail.com>
2024-05-16 08:31:06 -0400

    exemplo de funcao de expressão nomeada

---
+++ funcao/funcao-de-expressao-nomeada.js
@@ -0,0 +1,7 @@
+//Função de expressão nomeada para calcular o quadrado de um número
+const calcularQuadrado = function quadrado(numero) {
+    return numero * numero;
+};
+//Chamando a função para calcular o quadrado de um número
+const resultado = calcularQuadrado(5);
+console.log("Quadrado de 5 :" , resultado);
